947f82c071846f4fae8a05d0ece968186021daed
gajonuo <gajonuco@gmail.com>
2024-12-10 20:37:18 -0300

    Criando projeto InstagramBoft

---
+++ src/main/java/com/sistema/instagramBoft/InstagramBoftApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.instagramBoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramBoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramBoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/instagramBoft/InstagramBoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.instagramBoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
